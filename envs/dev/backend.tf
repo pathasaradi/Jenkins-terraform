d640dce5e7af550d9ea5f21b0adb3ca28b5b8545
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2025-12-07 17:47:07 +0530

    Remove dynamodb_table from backend.tf
    Removed the dynamodb_table configuration from the backend.

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "terraform-tf-backend"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "tf-locks"
     encrypt        = true
   }
 }
