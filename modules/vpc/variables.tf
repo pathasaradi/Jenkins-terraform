6886a4948770e8d0496c2f193df415df736c1d1e
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2025-12-07 17:37:04 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,19 @@
-variable "name" {}
-variable "vpc_cidr" {}
-variable "azs" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
+variable "name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
